43308d0728152f2772a5db714173ef2e855ecb6e
kyle <kp3crt@bolton.ac.uk>
2024-03-08 14:24:28 +0000

    Different menus now seem to open correctly allowing for a cleaner and easier to manage code base

--- MainMenu.py
+++ MainMenu.py
@@ -1,9 +1,14 @@
+import subprocess
 while True:
     try:
+        print("enter a number")
         menu = int(input())
         if menu == 1:
-            print("Menu 1")
+            print("Menu1")
+            exec(open('Menu1.py').read())
         elif menu == 2:
-            print("Menu 2")
+            print("Menu2")
+        elif menu == 0:
+            break
     except Exception:
         print("Error")
